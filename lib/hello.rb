223724e03c4996a494900031f07e24120ecbd806
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-03-04 12:36:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,12 @@
 def hello_t(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i += 1
-  end
-  array
+    if block_given?
+      i = 0
+      while i < array.length
+        yield array[i]
+        i += 1
+      end
+      array
+    end
 end
 
 # call your method here!
